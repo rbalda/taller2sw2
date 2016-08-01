e8d80defe8cac2eb79c05e993fe04e5a128a7c59
Leonardo <leoded94@hotmail.com>
2016-08-01 15:31:36 -0500

    Primera prueba

---
+++ acceso.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+def validarTarjeta(tarjeta):
+    #Tarjeta invalida
+    if len(tarjeta) != 7 or not tarjeta.isdigit():
+        return -1
+
+    #Tarjeta valida
+    header = tarjeta[0:2]
+    if "00" in header:
+        return 0
+    else:
+        return 1
+
+def acceso(tarjeta, date, dia):
+    lim_inf_est = datetime.strptime("08:00:00", "%H:%M:%S")
+    lim_sup_est = datetime.strptime("18:00:00", "%H:%M:%S")
+    lim_inf_emp = datetime.strptime("10:00:00", "%H:%M:%S")
+    lim_sup_emp = datetime.strptime("15:00:00", "%H:%M:%S")
+    new_date = datetime.strptime(date, "%H:%M:%S")
+
+    if dia not in range(1, 8) or validarTarjeta(tarjeta) == -1:
+        return 0
+
+    if validarTarjeta(tarjeta) == 0:
+        if dia in range(0, 6):
+            return 1
+        else:
+            if new_date > lim_inf_emp and new_date < lim_sup_emp:
+                return 1
+            else:
+                return 0
+    if validarTarjeta(tarjeta) == 1:
+        if dia in range(0, 6):
+            if new_date > lim_inf_est and new_date < lim_sup_est:
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+    return 0
+
---
+++ test.py
@@ -0,0 +1,43 @@
+import unittest
+from acceso import acceso
+
+class SimpleTestCase(unittest.TestCase):
+    def testA(self):
+        assert acceso("001024a", "11:30:32", 2) == 0
+        
+    def testB(self):
+        assert acceso("00102345", "11:30:32", 2) == 0
+    
+    def testC(self):
+        assert acceso("0010234", "11:30:32", -1) == 0
+
+    def testD(self):
+        assert acceso("0010234", "11:30:32", 8) == 0
+
+    def testE(self):
+        assert acceso("0010234", "11:30:32", 2) == 1
+
+    def testF(self):
+        assert acceso("0010234", "09:30:32", 7) == 0
+
+    def testG(self):
+        assert acceso("0010234", "16:30:32", 7) == 0
+
+    def testH(self):
+        assert acceso("0010234", "11:30:32", 7) == 1
+
+    def testI(self):
+        assert acceso("0110234", "07:30:32", 1) == 0
+
+    def testJ(self):
+        assert acceso("0110234", "11:30:32", 1) == 1
+
+    def testK(self):
+        assert acceso("0110234", "19:30:32", 1) == 0
+
+    def testJ(self):
+        assert acceso("0110234", "11:30:32", 7) == 0
+
+        
+if __name__ == "__main__":
+    unittest.main()
