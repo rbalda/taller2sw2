9a297dee3c44204d1a2932984ff5698ff2c70e51
J3S <johnnysuarez@outlook.com>
2016-08-02 16:19:39 -0500

    Agregado pruebas integracion fin

--- testintegration.py
+++ testintegration.py
@@ -27,7 +27,7 @@ class pruebas(unittest.TestCase):
         self.assertEqual(valor3, valor4)
 
     #Si la tarjeta es válida(empleado) y  todos los datos de día y hora son permitidos para el empleado, se permite el acceso
-    def test_integration2_module_1_2(self):
+    def test_integration3_module_1_2(self):
         programa = Programa()
         tarjeta = "0012345"
         valor1 = programa.modulo_1_verificar_tarjeta(tarjeta)
@@ -38,7 +38,7 @@ class pruebas(unittest.TestCase):
         valor4 = True
         self.assertEqual(valor3, valor4) # Verifica que el acceso fue concedido
     #Si la tarjeta es inválida y  todos los datos de día y hora son permitidos no se permite el acceso
-    def test_integration3_module_1_2(self):
+    def test_integration4_module_1_2(self):
         programa = Programa()
         tarjeta = "001234"
         valor1 = programa.modulo_1_verificar_tarjeta(tarjeta)
@@ -48,6 +48,38 @@ class pruebas(unittest.TestCase):
         valor3 = programa.modulo_2_acceso_valido(valor1[1], 2, 3)
         valor4 = False
         self.assertEqual(valor3, valor4) # Verifica que el acceso no fue concedido
+
+    #Si la tarjeta es válida(empleado) y  todos los datos de día y hora son permitidos para el empleado, se permite el acceso, se realiza el pago
+    def test_integration5_module_1_2_3(self):
+        programa = Programa()
+        tarjeta = ["3412345", "AAA", 1.0]
+        valor1 = programa.modulo_1_verificar_tarjeta(tarjeta[0])
+        valor2 = True
+        self.assertEqual(valor1[0], valor2) #Verifica que la tarjeta es válida
+
+        valor3 = programa.modulo_2_acceso_valido(valor1[1], 4, 10)
+        valor4 = True
+        self.assertEqual(valor3, valor4) # Verifica que el acceso fue concedido
+
+        valor5 = programa.modulo_3_sistema_pagos(tarjeta, 5)
+        valor6 = "Pasaje cobrado exitosamente"
+        self.assertEqual(valor5, valor6)
+
+    #Si la tarjeta es inválida y  todos los datos de día y hora son permitidos para el empleado, no se permite el acceso, no se realiza el pago
+    def test_integration6_module_1_2_3(self):
+        programa = Programa()
+        tarjeta = ["a", "AAA", 1.0]
+        valor1 = programa.modulo_1_verificar_tarjeta(tarjeta[0])
+        valor2 = False
+        self.assertEqual(valor1[0], valor2) #Verifica que la tarjeta es  noválida
+
+        valor3 = programa.modulo_2_acceso_valido(valor1[1], 4, 10)
+        valor4 = False
+        self.assertEqual(valor3, valor4) # Verifica que el acceso fue concedido
+
+        valor5 = programa.modulo_3_sistema_pagos(tarjeta, 5)
+        valor6 = "No se pudo cobrar el pasaje"
+        self.assertEqual(valor5, valor6)
 
 
 if __name__ == '__main__':
