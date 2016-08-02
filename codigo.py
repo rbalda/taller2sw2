bdf7836156b0a1ebfe560971ac3d6f55308456ac
vronikp <veronica.pozo@interprofcorp.com>
2016-08-02 10:26:13 -0500

    correccion

--- codigo.py
+++ codigo.py
@@ -14,13 +14,11 @@ class Codigo():
             return False
             
     def esEmpleado (self):
-        if (len(self.listaCodigo) ==7 and self.codigo.isdigit() and self.listaCodigo[0] == "0"
-            and self.listaCodigo[1]=="0"):
+        if (self.esValido() and self.listaCodigo[0] == "0" and self.listaCodigo[1]=="0"):
             return True
         return False
 
     def esEstudiante (self):
-        if (len(self.listaCodigo) ==7 and self.codigo.isdigit() and self.listaCodigo[0] != "0"
-            and self.listaCodigo[1]!="0"):
+        if (self.esValido() and self.listaCodigo[0] != "0" and self.listaCodigo[1]!="0"):
             return True
         return False
