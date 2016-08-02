7ef660ab3d707e6ba40f25c2df6838d13a5ce48a
Cliente CONTROL-PC <Cliente CONTROL-PC>
2016-08-02 16:42:10 -0500

    modulo 3 pago de pasaje

--- tarjetas_class.py
+++ tarjetas_class.py
@@ -1,28 +1,54 @@
 
 class Tarjeta():
 
-    def get_Tipo_Tarjeta(self, numero):
-        if(numero.isdigit()): # Only digit in this string
-            if(len(numero) == 7):
-                if(numero[0] == '0' and numero [1] == '0'):
+    nombres = ""
+    apellidos = ""
+    numero = ""
+    saldo = 0
+
+    def setSaldo(self, valor):
+        self.saldo = valor
+
+    def setNumero(self, valor):
+        self.numero = valor
+
+    def get_Tipo_Tarjeta(self):
+        if(self.numero.isdigit()): # Only digit in this string
+            if(len(self.numero) == 7):
+                if(self.numero[0] == '0' and self.numero [1] == '0'):
                     return "Empleado"
                 else:
                     return "Estudiante"
         return "Invalido"
 
-    def esHorarioValido(self, numeroT, dia, hora):
-        if(self.get_Tipo_Tarjeta(numeroT) == "Estudiante"):
+    def esHorarioValido(self, dia, hora):
+        if(self.get_Tipo_Tarjeta() == "Estudiante"):
             if(hora >= 8 and hora <= 18 and (dia == 1 or dia==2 or dia==3 or dia==4 or dia==5 or dia ==6 or dia ==7) ):
                 return 1
             else:
                 return 0
         else:
-            if(self.get_Tipo_Tarjeta(numeroT) == "Empleado"):
+            if(self.get_Tipo_Tarjeta() == "Empleado"):
                 if((hora >= 0 and hora <= 23.59 and (dia == 1 or dia==2 or dia==3 or dia==4 or dia==5)) or (hora >= 10 and hora <=15 and (dia == 6 or dia == 7) )):
                     return 1
                 else:
                     return 0
 
         return 0
+
+    def esPagoValido(self, dia):
+        if(dia == 5):
+            return 1
+        else:
+            if((dia == 1 or dia==2 or dia==3 or dia==4) and self.saldo > 0.25):
+                self.saldo = self.saldo - 0.25
+                return 1
+            else:
+                return 0
+        return 0
+
+
+
+
 
 
--- testTarjeta.py
+++ testTarjeta.py
@@ -1,63 +1,104 @@
-
-
 import unittest
 from tarjetas_class import Tarjeta
 
+
 class Test(unittest.TestCase):
-    def test1(self):  #codigo de acceso valor entero
+    def test1(self):  # codigo de acceso valor entero
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 1, 10), 1)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(1, 10), 1)
 
-    def test2(self):  #codigo de acceso valor entero de 7 digitos
+    def test2(self):  # codigo de acceso valor entero de 7 digitos
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 1, 10), 1)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(1, 10), 1)
 
-    def test3(self):  #codigo de acceso valor entero no tiene 7 digitos
+    def test3(self):  # codigo de acceso valor entero no tiene 7 digitos
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('00123', 1, 10), 0)
+        t.setNumero("00123")
+        self.assertEquals(t.esHorarioValido(1, 10), 0)
 
-    def test4(self):  #codigo de acceso valor entero
+    def test4(self):  # codigo de acceso valor entero
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 1, 10), 1)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(1, 10), 1)
 
-    def test5(self):  #codigo de acceso no es valor entero
+    def test5(self):  # codigo de acceso no es valor entero
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('hola', 1, 10), 0)
+        t.setNumero("hola")
+        self.assertEquals(t.esHorarioValido(1, 10), 0)
 
-    def test6(self):  #dia valor entero
+    def test6(self):  # dia valor entero
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('1012345', 1, 10), 1)
+        t.setNumero("1012345")
+        self.assertEquals(t.esHorarioValido(1, 10), 1)
 
-    def test7(self):  #dia valor ENTERO ENTre 1 y 7
+    def test7(self):  # dia valor ENTERO ENTre 1 y 7
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('1012345', 1, 6), 0)
+        t.setNumero("1012345")
+        self.assertEquals(t.esHorarioValido(1, 6), 0)
 
-    def test8(self):  #dia menor a 1
+    def test8(self):  # dia menor a 1
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 0, 10), 0)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(0, 10), 0)
 
-    def test9(self):  #dia no está entre 1 y 7
+    def test9(self):  # dia no está entre 1 y 7
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 0, 10), 0)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(0, 10), 0)
 
-    def test10(self):  #dia mayor a 7
+    def test10(self):  # dia mayor a 7
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 10, 10), 0)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(10, 10), 0)
 
-    def test11(self):  #hora entre 24 horas
+    def test11(self):  # hora entre 24 horas
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 1, 10), 1)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(1, 10), 1)
 
-    def test12(self):  #hora no está entre 24 horas
+    def test12(self):  # hora no está entre 24 horas
         t = Tarjeta()
-        self.assertEquals(t.esHorarioValido('0012345', 10, 24), 0)
+        t.setNumero("0012345")
+        self.assertEquals(t.esHorarioValido(10, 24), 0)
+
+    def test13(self):
+        t = Tarjeta()
+        t.setSaldo(0)
+        self.assertEquals(t.esPagoValido(5), 1)
+
+    def test14(self):
+        t = Tarjeta()
+        t.setSaldo(0.5)
+        self.assertEquals(t.esPagoValido(1,), 1)
+
+    def test15(self):
+        t = Tarjeta()
+        t.setSaldo(0)
+        self.assertEquals(t.esPagoValido(1), 0)
+
+    def test16(self):
+        t = Tarjeta()
+        t.setSaldo(0)
+        self.assertEquals(t.esPagoValido(7), 0)
+
+    def test17(self):
+        t = Tarjeta()
+        t.setSaldo(0)
+        self.assertEquals(t.esPagoValido(1.5), 0)
+
+    def test18(self):
+        t = Tarjeta()
+        t.setSaldo(-0.5)
+        self.assertEquals(t.esPagoValido(1), 0)
+
+    def test19(self):
+        t = Tarjeta()
+        t.setSaldo(0)
+        self.assertEquals(t.esPagoValido(-1), 0)
 
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
