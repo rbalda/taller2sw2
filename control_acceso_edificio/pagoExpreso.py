9482d212cd57b7b39409a5d909dd7dc80ecfb41f
Jorge Santiago Ayala Sáenz <Jorge Santiago Ayala Senz>
2016-08-02 15:25:34 -0500

    Prueba actividad 2

---
+++ control_acceso_edificio/pagoExpreso.py
@@ -0,0 +1,28 @@
+from validorTarjetas import Validador
+
+class sistemaPago():
+	"""
+	Autor: Jorge Ayala
+	
+	"""
+	def respuesta(self,apellido,nombre,idTarjeta,saldo,dia):
+                validar = Validador()
+                tipoUsuario = validar.respuesta(idTarjeta,24,181,0)
+                if(tipoUsuario == None):
+                        return 0
+                else:
+                        if(dia >= 1 and dia <=4):
+                                if(saldo >= 0.25):
+                                        saldo = saldo - 0.25
+                                        return 1
+                                else:
+                                        return 0
+                        elif(dia == 5):
+                                return 1
+                        else:
+                                return 0
+                                
+                        
+                
+                        
+            
